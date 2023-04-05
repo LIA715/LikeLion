895a464b53a64020e33b79af1c08eceaaa886e76
seoyean <sy0715p@naver.com>
2023-04-05 22:45:12 +0900

    04.05 ~챕터 5.

--- src/PrimitiveType.java
+++ src/PrimitiveType.java
@@ -3,5 +3,7 @@ public class PrimitiveType {
         //정수형
         //1. byte (1byte = 8bits) : -2^7 ~ 2^7-1(-128~127)
         //2. long (2bytes) : -2^15 ~ 2^15-1(-32,768 ~32767)
+        //3. int (4bytes) : -2^31 ~ 2^31-1
+        //4. long (8bytes) : -2^63 ~ 2^63-1
     }
 }
