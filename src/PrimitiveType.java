34d9d0893df20b366b25651ff78c58451022518e
seoyean <sy0715p@naver.com>
2023-04-05 22:55:05 +0900

    04.05 ~챕터 5.

--- src/PrimitiveType.java
+++ src/PrimitiveType.java
@@ -5,5 +5,8 @@ public class PrimitiveType {
         //2. long (2bytes) : -2^15 ~ 2^15-1(-32,768 ~32767)
         //3. int (4bytes) : -2^31 ~ 2^31-1
         //4. long (8bytes) : -2^63 ~ 2^63-1
+
+        byte a = 120; //범위에 맞지 않은 숫자를 넣으면 오류 발생
+
     }
 }
