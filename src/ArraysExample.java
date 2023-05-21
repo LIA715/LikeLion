6242c1ea34c425db8363edce6e9c852e79bc10ce
LIA715 <sy0715p@naver.com>
2023-05-21 22:01:19 +0900

    챕터 8

--- src/ArraysExample.java
+++ src/ArraysExample.java
@@ -6,11 +6,20 @@ public class ArraysExample {
         int[] price = {10000,9000,40000,7000};
         String[] mbti = {"INFP","ENFP","ISTJ","ESTP"};
 
-        System.out.println(price[0]);
-        System.out.println(mbti[0]);
+        System.out.println(mbti.length);
 
-        price[1] = 8000; //값 덮어쓰기
-        System.out.println(price[1]);
+        for (int i = 0; i < mbti.length; i++){
+            System.out.println(mbti[i]);
+        }
+
+
+//        System.out.println(price[0]);
+//        System.out.println(mbti[0]);
+//
+//        price[1] = 8000; //값 덮어쓰기
+//        System.out.println(price[1]);
+
+
 
 
 
